7dd3276eaaddfff5486cd369e8d5b723b046f2a2
ink16 <rares@jekyo.com>
2021-10-12 13:24:59 +0300

    created umijs demo

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import styles from './index.less';
+
+export default function IndexPage() {
+  return (
+    <div>
+      <h1 className={styles.title}>UmiJS app deployed on Jekyo</h1>
+    </div>
+  );
+}
